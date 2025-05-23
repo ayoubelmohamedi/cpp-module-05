4f31ef58a04b53e96b225eea12e7d8c2c14674b2
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-23 22:36:28 +0100

    Make 'what' method virtual in inherited exception class

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -31,13 +31,13 @@ class Bureaucrat
         class GradeTooHighException : public std::exception
         {
             public:
-                const char *what() const throw();
+                virtual const char *what() const throw();
         };
 
         class GradeTooLowException : public std::exception
         {
             public:
-                const char *what() const throw();
+                virtual const char *what() const throw();
         };
 };
 
